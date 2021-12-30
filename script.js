9b6938e97de64a5da5718388e98b50001030f0bc
Jeylen <jnpj101@gmail.com>
2021-12-30 23:48:09 +0000

    changed javascript function

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-
 function compute(){
-
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 }
